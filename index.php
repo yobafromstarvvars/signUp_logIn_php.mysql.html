98f0b048fd0118dbd2c9438ed16abec9efc838cb
yobafromstarvvars <skavronsky.vt@gmail.com>
2023-03-01 01:03:15 +0300

    sign in and log in backend completed

--- index.php
+++ index.php
@@ -1,33 +1,49 @@
+<?php
+// start session
+// pass user id to the session
+// go to home page
+// HOME PAGE
+// continue session
+// if id is empty, suggest to log in or sign up
+// if id is not empty, suggest to log out - session_destroy()
+// login page - session_regenerate_id()
+session_start();
+if (isset($_SESSION["user_id"])) {
+$mysqli = require __DIR__ . "/database.php";
+$sql = sprintf("SELECT * FROM user 
+                WHERE id = '%s'", 
+                $_SESSION["user_id"]);
+$result = $mysqli->query($sql);
+$user = $result->fetch_assoc();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Sign Up</title>
+  <title>Home</title>
   <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">-->
   <link rel="stylesheet" href="styles/water.css">
 </head>
 <body>
-  <h1>Sign Up</h1>
-  <form action="process-signup.php" method="POST">
-      <div>
-          <label for="name">Name</label>
-          <input id="name" name="name" type="text" required>
-      </div>
-      <div>
-          <label for="email">Email</label>
-          <input id="email" type="email" name="email" required>
-      </div>
-      <div>
-          <label for="password">Password</label>
-          <input id="password" type="password" name="password" required>
-      </div>
-      <div>
-          <label for="password_repeat">Repeat password</label>
-          <input id="password_repeat" type="password" name="password_repeat" required>
-      </div>
-     
-      <button type="submit">Sign Up</button>
-  </form>
+  <h1>Home</h1>
+
+    <?php if (isset($user)): ?>
+
+      <p>Hello, <?= htmlspecialchars($user["name"]) ?></p>
+      <a href="logout.php">Log Out</a>
+
+    <?php else: ?>
+
+      <p>
+        <a href="login.php">Log in</a> 
+        or 
+        <a href="signup.php">Sign Up</a>
+      </p>
+    
+    <?php endif; ?>
+
 </body>
 </html>
--- process-signup.php
+++ process-signup.php
@@ -34,5 +34,26 @@ if ($_POST["password"] !== $_POST["password_repeat"]) {
 
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-print_r($_POST);
-var_dump($password_hash);
+//print_r($_POST);
+//var_dump($password_hash);
+
+$mysqli = require __DIR__ . "/database.php";
+$sql = "INSERT INTO user (name, email, password) VALUES (?,?,?)";
+$stmt = $mysqli->stmt_init();
+if (! $stmt->prepare($sql)) {
+    die("Query error: " . $mysqli->error);
+}
+$stmt->bind_param("sss", $_POST["name"], $_POST["email"], $password_hash);
+
+// Show error if user already exists
+if ($stmt->execute()){
+    //user doesn't exist
+    header("Location: signup-successful.html");
+    exit;
+} else {
+    if ($mysqli->errno === 1062) {
+        die("Email is already taken");
+    }
+    die("Insert data error: " . $mysqli->error . $mysqli->errno);
+}
+
